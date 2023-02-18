f6a6479aff28f5450d01b8262f41463f03a4c615
ctang415 <carrietang117@gmail.com>
2023-02-18 18:12:55 -0500

    Created test case for function to shuffle an array

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('shuffle array', () => {
+  let villagers = ['pietro', 'eunice', 'chrissy', 'broccolo', 'dizzy', 'marshal', 'merengue', 'nibbles']
+  let villagersOriginal = ['pietro', 'eunice', 'chrissy', 'broccolo', 'dizzy', 'marshal', 'merengue', 'nibbles']
+  function shuffleArray(arr) {
+    let i, j, index
+    for (index = arr.length-1; index > 0; index--) {
+      j = Math.floor(Math.random() * (index + 1))
+      i = arr[index]
+      arr[index] = arr[j]
+      arr[j] = i
+    }
+    return arr
+  }
+  expect(shuffleArray(villagers)).not.toEqual(villagersOriginal);
 });
+
