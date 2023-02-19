4a1240e42a90935b0f577b085e5eb43530969b04
ctang415 <carrietang117@gmail.com>
2023-02-19 10:38:59 -0500

    Added some css to header section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import Card from './components/Card'
+import { Broccolo, Cousteau, Chester, Dizzy, Francine, Eunice, Ketchup, Lily, 
+         Maple, Marshal, Merengue, Nibbles, Pietro, Raymond, Roald, Sherb } from "./images";
 
+  const App = () => {
+    const images = [ Broccolo, Cousteau, Chester, Dizzy, Francine, Eunice, Ketchup, Lily, 
+      Maple, Marshal, Merengue, Nibbles, Pietro, Raymond, Roald, Sherb ]
+    
+    const [score, setScore] = useState(0)
 
-function App() {
-  return (
-    <div>
+    const [record, setRecord] = useState(0)
+
+    const [cards, setCards] = useState({
+      photo: Broccolo,
+      name: ''
+    })
+
+    const shuffleArray = (array) => {
+      let i, j, index;
+      for (index = array.length-1; index > 0; index--) {
+        j = Math.floor(Math.random()*(index+1))
+        i = array[index]
+        array[index] = array[j]
+        array[j] = i
+      }
+      return array
+    }
+
+    useEffect(() => {
+      shuffleArray(cards)
+    }, [])
+  
+    return (
+      <div>
+        <div className="header">
+          <div className="left">
+          <span id="headertext" className="title">Animal Crossing Memory Game</span>
+          <span id="instruction">How to Play: Get points by clicking on each card once. 
+          Make sure you don't click on the same one twice!</span>
+        </div>
+        <div className="right">
+          <span>Current Score: {score}</span>
+          <span>High Score: {record}</span>
+        </div>
+          <Card
+          cards={cards}
+          />
+        </div>
     </div>
   );
 }
